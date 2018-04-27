6aed25131252d617b5d8e5e2f2ea1c3d1cadabcf
Andreas Jacob <cordobo@web.de>
2018-04-28 01:14:22 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+export class NgxQrCode {
+  text: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  public qrdata: string = null;
+
+  constructor () {
+    console.log('AppComponent running');
+    this.qrdata = 'Initial QR code data string';
+  }
+
+  changeValue( newValue: string): void {
+    this.qrdata = newValue;
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+
+import { QRCodeModule } from 'angularx-qrcode';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    QRCodeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
