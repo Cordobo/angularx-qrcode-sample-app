1cb3344e2f3838b02f81969bd3c0226813988336
Andreas Jacob <cordobo@web.de>
2020-06-05 19:55:47 +0200

    Fix for SVG width

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,35 @@ export class AppComponent {
 
   public qrdata: string = null;
   public elementType: "img" | "url" | "canvas" | "svg" = null;
-  public scale: number = 1;
+  public level: "L" | "M" | "Q" | "H";
+  public scale: number;
+  public width: number;
 
   constructor() {
-    this.qrdata = "Initial QR code data string";
     this.elementType = "img";
-    this.scale = 4;
-  }
-
-  changeValue(newValue: string): void {
-    this.qrdata = newValue;
+    this.level = "M";
+    this.qrdata = "Initial QR code data string";
+    this.scale = 1;
+    this.width = 256;
   }
 
   changeElementType(newValue: "img" | "url" | "canvas" | "svg"): void {
     this.elementType = newValue;
+  }
+
+  changeLevel(newValue: "L" | "M" | "Q" | "H"): void {
+    this.level = newValue;
+  }
+
+  changeQrdata(newValue: string): void {
+    this.qrdata = newValue;
+  }
+
+  changeScale(newValue: number): void {
+    this.scale = newValue;
+  }
+
+  changeWidth(newValue: number): void {
+    this.width = newValue;
   }
 }
